01bfc6d187f6a12292713b99783030589ac606d6
MAgi-15 <3103120140@student.smktelkom-pwt.sch.id>
2022-11-07 15:11:51 +0700

    auth

--- routes/api.php
+++ routes/api.php
@@ -36,3 +36,19 @@ Route::resource('books', BookController::class)->except(
 Route::resource('authors', AuthorController::class)->except(
     ['create', 'edit']
 );
+
+//public route
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/books', [BookController::class, 'index']);
+Route::get('/books/{id}', [BookController::class, 'show']);
+Route::get('/Authors', [AuthorController::class, 'index']);
+Route::get('/Authors/{id}', [AuthorController::class, 'show']);
+
+//protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::resource('books', BookController::class)->except('create', 'edit', 'show', 'index');
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::resource('authors', AuthorController::class)->except('create', 'edit', 'show', 'index');
+});
